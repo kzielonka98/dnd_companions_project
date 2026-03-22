b20e1e6fed8a8a7a8c87685da730ea01567e5b8a
k.zielonka <k.zielonka1998@gmail.com>
2026-03-22 21:11:43 +0100

    fix: update Playwright configuration for local server and adjust test URL

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,21 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-const mainUrl = 'http://localhost:5109/'
 
 test('has title', async ({ page }) => {
-  await page.goto(mainUrl);
+  await page.goto("");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Home Page - DndCompanion/);
 
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });

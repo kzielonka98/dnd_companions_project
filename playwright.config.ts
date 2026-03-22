a1e3322d2fec0d4e4d7407acd40223ab2984927f
k.zielonka <k.zielonka1998@gmail.com>
2026-03-22 21:18:20 +0100

    fix: comment out local web server configuration in Playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-     command: 'dotnet run --project DndCompanion/',
-     url: 'http://localhost:5109',
-     reuseExistingServer: !process.env.CI,
-   },
+  // webServer: {
+  //    command: 'npm run start',
+  //    url: 'http://localhost:5109',
+  //    reuseExistingServer: !process.env.CI,
+  //  },
 });
